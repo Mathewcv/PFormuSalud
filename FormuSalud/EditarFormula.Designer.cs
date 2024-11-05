f00e6749a87c0291a8cacd7314ffa72b7579b1b2
Matthew <126118946+MattCV@users.noreply.github.com>
2024-11-04 20:52:55 -0500

    editar formula medica falta funcionalidad

---
+++ FormuSalud/EditarFormula.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace FormuSalud
+{
+    partial class EditarFormula
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFirmar = new System.Windows.Forms.Button();
+            this.pbFirma = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dateFecha = new System.Windows.Forms.DateTimePicker();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.txtAnotaciones = new System.Windows.Forms.TextBox();
+            this.lblAnotaciones = new System.Windows.Forms.Label();
+            this.txtObservaciones = new System.Windows.Forms.TextBox();
+            this.lblObservaciones = new System.Windows.Forms.Label();
+            this.txtMedicamentos = new System.Windows.Forms.TextBox();
+            this.lblMedicamentos = new System.Windows.Forms.Label();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.lblDocumento = new System.Windows.Forms.Label();
+            this.txtDoctor = new System.Windows.Forms.TextBox();
+            this.lblDoctor = new System.Windows.Forms.Label();
+            this.txtHora = new System.Windows.Forms.TextBox();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPaciente = new System.Windows.Forms.TextBox();
+            this.lblPaciente = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.txtNumH = new System.Windows.Forms.TextBox();
+            this.lblHistoria = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.lblEdad = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFirma)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFirmar
+            // 
+            this.btnFirmar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
+            this.btnFirmar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFirmar.ForeColor = System.Drawing.Color.White;
+            this.btnFirmar.Location = new System.Drawing.Point(662, 173);
+            this.btnFirmar.Name = "btnFirmar";
+            this.btnFirmar.Size = new System.Drawing.Size(167, 41);
+            this.btnFirmar.TabIndex = 75;
+            this.btnFirmar.Text = "Cargar Firma";
+            this.btnFirmar.UseVisualStyleBackColor = false;
+            // 
+            // pbFirma
+            // 
+            this.pbFirma.Location = new System.Drawing.Point(399, 168);
+            this.pbFirma.Name = "pbFirma";
+            this.pbFirma.Size = new System.Drawing.Size(235, 183);
+            this.pbFirma.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbFirma.TabIndex = 74;
+            this.pbFirma.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FormuSalud.Properties.Resources.logo_soloblanco_Photoroom;
+            this.pictureBox1.Location = new System.Drawing.Point(699, 274);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 153);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 73;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dateFecha
+            // 
+            this.dateFecha.Location = new System.Drawing.Point(116, 117);
+            this.dateFecha.Name = "dateFecha";
+            this.dateFecha.Size = new System.Drawing.Size(200, 20);
+            this.dateFecha.TabIndex = 72;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(429, 372);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(233, 55);
+            this.btnEditar.TabIndex = 71;
+            this.btnEditar.Text = " Editar Formula";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            // 
+            // txtAnotaciones
+            // 
+            this.txtAnotaciones.Location = new System.Drawing.Point(507, 115);
+            this.txtAnotaciones.Name = "txtAnotaciones";
+            this.txtAnotaciones.Size = new System.Drawing.Size(322, 20);
+            this.txtAnotaciones.TabIndex = 70;
+            // 
+            // lblAnotaciones
+            // 
+            this.lblAnotaciones.AutoSize = true;
+            this.lblAnotaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnotaciones.Location = new System.Drawing.Point(396, 115);
+            this.lblAnotaciones.Name = "lblAnotaciones";
+            this.lblAnotaciones.Size = new System.Drawing.Size(105, 16);
+            this.lblAnotaciones.TabIndex = 69;
+            this.lblAnotaciones.Text = "Anotaciones : ";
+            // 
+            // txtObservaciones
+            // 
+            this.txtObservaciones.Location = new System.Drawing.Point(617, 78);
+            this.txtObservaciones.Name = "txtObservaciones";
+            this.txtObservaciones.Size = new System.Drawing.Size(212, 20);
+            this.txtObservaciones.TabIndex = 68;
+            // 
+            // lblObservaciones
+            // 
+            this.lblObservaciones.AutoSize = true;
+            this.lblObservaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblObservaciones.Location = new System.Drawing.Point(396, 78);
+            this.lblObservaciones.Name = "lblObservaciones";
+            this.lblObservaciones.Size = new System.Drawing.Size(215, 16);
+            this.lblObservaciones.TabIndex = 67;
+            this.lblObservaciones.Text = "Observaciones del Paciente : ";
+            // 
+            // txtMedicamentos
+            // 
+            this.txtMedicamentos.Location = new System.Drawing.Point(174, 325);
+            this.txtMedicamentos.Name = "txtMedicamentos";
+            this.txtMedicamentos.Size = new System.Drawing.Size(152, 20);
+            this.txtMedicamentos.TabIndex = 66;
+            // 
+            // lblMedicamentos
+            // 
+            this.lblMedicamentos.AutoSize = true;
+            this.lblMedicamentos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedicamentos.Location = new System.Drawing.Point(48, 325);
+            this.lblMedicamentos.Name = "lblMedicamentos";
+            this.lblMedicamentos.Size = new System.Drawing.Size(120, 16);
+            this.lblMedicamentos.TabIndex = 65;
+            this.lblMedicamentos.Text = "Medicamentos : ";
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(151, 257);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(108, 20);
+            this.txtDocumento.TabIndex = 64;
+            // 
+            // lblDocumento
+            // 
+            this.lblDocumento.AutoSize = true;
+            this.lblDocumento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDocumento.Location = new System.Drawing.Point(48, 257);
+            this.lblDocumento.Name = "lblDocumento";
+            this.lblDocumento.Size = new System.Drawing.Size(97, 16);
+            this.lblDocumento.TabIndex = 63;
+            this.lblDocumento.Text = "Documento : ";
+            // 
+            // txtDoctor
+            // 
+            this.txtDoctor.Location = new System.Drawing.Point(130, 292);
+            this.txtDoctor.Name = "txtDoctor";
+            this.txtDoctor.Size = new System.Drawing.Size(150, 20);
+            this.txtDoctor.TabIndex = 62;
+            // 
+            // lblDoctor
+            // 
+            this.lblDoctor.AutoSize = true;
+            this.lblDoctor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDoctor.Location = new System.Drawing.Point(48, 292);
+            this.lblDoctor.Name = "lblDoctor";
+            this.lblDoctor.Size = new System.Drawing.Size(80, 16);
+            this.lblDoctor.TabIndex = 61;
+            this.lblDoctor.Text = "Doctor@ : ";
+            // 
+            // txtHora
+            // 
+            this.txtHora.Location = new System.Drawing.Point(116, 153);
+            this.txtHora.Name = "txtHora";
+            this.txtHora.Size = new System.Drawing.Size(91, 20);
+            this.txtHora.TabIndex = 60;
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.Location = new System.Drawing.Point(48, 153);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(53, 16);
+            this.lblHora.TabIndex = 59;
+            this.lblHora.Text = "Hora : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 31);
+            this.label1.TabIndex = 58;
+            this.label1.Text = " Editar Fórmula Médica ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtPaciente
+            // 
+            this.txtPaciente.Location = new System.Drawing.Point(130, 185);
+            this.txtPaciente.Name = "txtPaciente";
+            this.txtPaciente.Size = new System.Drawing.Size(150, 20);
+            this.txtPaciente.TabIndex = 57;
+            // 
+            // lblPaciente
+            // 
+            this.lblPaciente.AutoSize = true;
+            this.lblPaciente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaciente.Location = new System.Drawing.Point(48, 186);
+            this.lblPaciente.Name = "lblPaciente";
+            this.lblPaciente.Size = new System.Drawing.Size(76, 16);
+            this.lblPaciente.TabIndex = 56;
+            this.lblPaciente.Text = "Paciente :";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(48, 117);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(62, 16);
+            this.lblFecha.TabIndex = 55;
+            this.lblFecha.Text = "Fecha : ";
+            // 
+            // txtNumH
+            // 
+            this.txtNumH.Location = new System.Drawing.Point(213, 82);
+            this.txtNumH.Name = "txtNumH";
+            this.txtNumH.Size = new System.Drawing.Size(113, 20);
+            this.txtNumH.TabIndex = 54;
+            // 
+            // lblHistoria
+            // 
+            this.lblHistoria.AutoSize = true;
+            this.lblHistoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHistoria.Location = new System.Drawing.Point(48, 82);
+            this.lblHistoria.Name = "lblHistoria";
+            this.lblHistoria.Size = new System.Drawing.Size(159, 16);
+            this.lblHistoria.TabIndex = 53;
+            this.lblHistoria.Text = "Num Historia Clinica : ";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(116, 222);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(52, 20);
+            this.txtEdad.TabIndex = 52;
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.Location = new System.Drawing.Point(48, 222);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(52, 16);
+            this.lblEdad.TabIndex = 51;
+            this.lblEdad.Text = "Edad :";
+            // 
+            // EditarFormula
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(221)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(921, 450);
+            this.Controls.Add(this.btnFirmar);
+            this.Controls.Add(this.pbFirma);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dateFecha);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.txtAnotaciones);
+            this.Controls.Add(this.lblAnotaciones);
+            this.Controls.Add(this.txtObservaciones);
+            this.Controls.Add(this.lblObservaciones);
+            this.Controls.Add(this.txtMedicamentos);
+            this.Controls.Add(this.lblMedicamentos);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.lblDocumento);
+            this.Controls.Add(this.txtDoctor);
+            this.Controls.Add(this.lblDoctor);
+            this.Controls.Add(this.txtHora);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPaciente);
+            this.Controls.Add(this.lblPaciente);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.txtNumH);
+            this.Controls.Add(this.lblHistoria);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.lblEdad);
+            this.Name = "EditarFormula";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EditarFormula";
+            ((System.ComponentModel.ISupportInitialize)(this.pbFirma)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFirmar;
+        private System.Windows.Forms.PictureBox pbFirma;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dateFecha;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.TextBox txtAnotaciones;
+        private System.Windows.Forms.Label lblAnotaciones;
+        private System.Windows.Forms.TextBox txtObservaciones;
+        private System.Windows.Forms.Label lblObservaciones;
+        private System.Windows.Forms.TextBox txtMedicamentos;
+        private System.Windows.Forms.Label lblMedicamentos;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.Label lblDocumento;
+        private System.Windows.Forms.TextBox txtDoctor;
+        private System.Windows.Forms.Label lblDoctor;
+        private System.Windows.Forms.TextBox txtHora;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPaciente;
+        private System.Windows.Forms.Label lblPaciente;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.TextBox txtNumH;
+        private System.Windows.Forms.Label lblHistoria;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label lblEdad;
+    }
+}
